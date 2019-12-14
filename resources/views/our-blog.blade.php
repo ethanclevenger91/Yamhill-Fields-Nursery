4599898179e450403789695d3e1228cb3ddb003f
Logan Testa <logan.testa@outlook.com>
2019-12-13 17:13:06 -0800

    Add blog styling

--- resources/views/our-blog.blade.php
+++ resources/views/our-blog.blade.php
@@ -61,7 +61,7 @@ declare(strict_types = 1);
                                         ?>
                                     </div>
                                     <div class="blog__date"><?php the_date(); ?></div>
-                                    <div class="blog__image"><?php the_post_thumbnail(); ?></div>
+                                    <?php if(has_post_thumbnail()) { ?><div class="blog__image"><?php the_post_thumbnail(); ?></div><?php } ?>
                                     <div class="blog__content"><?php the_content(); ?></div>
                                     <div class="clear-both"></div>
                                 </div>
