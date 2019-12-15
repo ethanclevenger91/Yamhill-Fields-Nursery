cbf089134469dc9f237bfabf4677b91959fca632
Logan Testa <logan.testa@outlook.com>
2019-12-14 17:46:32 -0800

    Adjust nav structure

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,20 +1,17 @@
   
 <nav class="nav mobile-nav" id="mobileNav">
-    <div class="inner-wrapper">
-        <div id="dropdownButton"></div>
-        <div id="dropdownContent">
-            <ul class="nav__nav-list">
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/index">Home</a>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/plants">Plants</a>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/supplies">Supplies</a>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/our-blog">Blog</a>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/about-us">About</a>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/contact-us">Contact</a>
-            </ul>
-        </div>
+    <div id="dropdownButton"></div>
+    <div id="dropdownContent">
+        <ul class="nav__nav-list">
+            <li class="nav__nav-item"><a class="nav__nav-link" href="/index">Home</a>
+            <li class="nav__nav-item"><a class="nav__nav-link" href="/plants">Plants</a>
+            <li class="nav__nav-item"><a class="nav__nav-link" href="/supplies">Supplies</a>
+            <li class="nav__nav-item"><a class="nav__nav-link" href="/our-blog">Blog</a>
+            <li class="nav__nav-item"><a class="nav__nav-link" href="/about-us">About</a>
+            <li class="nav__nav-item"><a class="nav__nav-link" href="/contact-us">Contact</a>
+        </ul>
     </div>
 </nav>
-
 
 
 <nav class="nav desktop-nav" id="desktopNav">
