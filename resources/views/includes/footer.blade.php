74ed1c48966f4d473a499759b02724b5d0ad5ecb
Logan Testa <logan.testa@outlook.com>
2019-12-14 17:04:47 -0800

    Adjsut footer styling

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,26 +1,28 @@
 <footer class="footer">
     <div class="footer__background"></div>
-    <div class="footer__copyright">
-	   Copyright &copy; <?php echo date("Y"); ?> Yamhill Fields Nursery, LLC.  All rights reserved.
+    <div class="inner-wrapper">
+        <div class="footer__copyright">
+            Copyright &copy; <?php echo date("Y"); ?> Yamhill Fields Nursery, LLC.  All rights reserved.
+        </div>
+        <div class="footer__location">
+            <div class="footer__location-header">Location and Hours:</div> 
+            <div class="footer__location-address">45202 SW Dundee, OR 97115</div>
+            <div class="footer__location-hours">Open Mon-Sat 9:00 am-9:00 pm, closed Sunday.</div>
+            <div class="footer__phone">503-538-8204</div>
+        </div>	
+        <div class="footer__social">
+            <div class="footer__social-item">
+                <a href="/" class="footer__social-link">
+                    <i class="fab fa-facebook-f fa-2x footer__social-icon"><span class="sr-only">Facebook</span></i>
+                </a>
+            </div>
+            <div class="footer__social-item">
+                <a href="/" class="footer__social-link">
+                    <i class="fab fa-pinterest fa-2x footer__social-icon"><span class="sr-only">Pinterest</span></i>
+                </a>
+            </div>
+        </div>
     </div>
-	<div class="footer__location">
-	    <div class="footer__location-header">Location and Hours:</div> 
-	    <div class="footer__location-address">45202 SW Dundee, OR 97115</div>
-	    <div class="footer__location-hours">Open Mon-Sat 9:00 am-9:00 pm, closed Sunday.</div>
-	    <div class="footer__phone">503-538-8204</div>
-	</div>	
-	<div class="footer__social">
-	    <div class="footer__social-item">
-		    <a href="/" class="footer__social-link">
-			    <i class="fab fa-facebook-f fa-2x footer__social-icon"><span class="sr-only">Facebook</span></i>
-		    </a>
-	    </div>
-		<div class="footer__social-item">
-		    <a href="/" class="footer__social-link">
-			    <i class="fab fa-pinterest fa-2x footer__social-icon"><span class="sr-only">Pinterest</span></i>
-			</a>
-	    </div>
-	</div>
 </footer>
 <script type="text/javascript" src="/assets/javascript/javascript-functions.js"></script>
 <script type="text/javascript" src="/assets/javascript/vue-javascript.js"></script>
