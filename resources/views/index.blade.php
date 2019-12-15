7ce0be598869a799b5b26fb69e9014b7477a9f52
Logan Testa <logan.testa@outlook.com>
2019-12-14 16:41:14 -0800

    Adjust index layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,72 +21,76 @@
             @include('includes.logo')
             @include('includes.nav')
             <div class="content">
-                <div class="content-row">
-                    <div class="col-sma-5">
-                        <h3>The Best Nursery in Yamhill County</h3>
-                        <p class="content__highlighted-phrase"><strong>Get all your gardening and landscaping plants and supplies in one stop!</strong></p>
-                        <p>Grass, Shrubs, House Plants, and Saplings.</p>
-                        <p>Gardening and Landscaping Tools.</p>
-                        <p>Open Mon-Sat 9:00 am-9:00 pm, closed Sunday. Questions: call us at 503-538-8204.</p>
-                        <p>Located near Portland in the Willamette Valley in Dundee.</p>
+                <div class="inner-wrapper">
+                    <div class="content-row">
+                        <div class="col-sma-5">
+                            <h3>The Best Nursery in Yamhill County</h3>
+                            <p class="content__highlighted-phrase"><strong>Get all your gardening and landscaping plants and supplies in one stop!</strong></p>
+                            <p>Grass, Shrubs, House Plants, and Saplings.</p>
+                            <p>Gardening and Landscaping Tools.</p>
+                            <p>Open Mon-Sat 9:00 am-9:00 pm, closed Sunday. Questions: call us at 503-538-8204.</p>
+                            <p>Located near Portland in the Willamette Valley in Dundee.</p>
+                        </div>
+                        <div class="col-sma-7">
+                            <div class="index-content-image one"></div>		     
+                        </div>
                     </div>
-                    <div class="col-sma-7">
-                        <div class="index-content-image one"></div>		     
-                    </div>
-                </div>
-                <div class="content-row">
-                    <div class="col-sma-1">&nbsp;</div>
-                    <div class="col-sma-10">
-                        <h3>Serving Gardners and Landscapers Since 1982</h3>
-                        <p>We've had the honor of selling plants and landscaping and gardening supplies to our customers for many years.
-                            Drop and by and see why garderners and landscapers make us their go-to garden and landscaping store!</p>
-                    </div>
-                    <div class="col-sma-1">&nbsp;</div>
-                </div>	
-                <div class="content-row">
-                    <div class="col-sma-12">
-                        <?php
-                        global $post;
-                        $args = array('posts_per_page' => 3);
-                        $postsToDisplay = get_posts($args);
-                        foreach ($postsToDisplay as $post) : setup_postdata($post);
-                            ?>      
-                            <div class="col-sma-4">
-                                <div class="blog-post">
-                                    <a href="/our-blog"><h4 class="blog-post__title"><?php the_title(); ?></h4></a>
-                                    <div class="blog__categories"><?php
-                                        $categories = get_the_category();
-                                        $h = 0;
-                                        foreach ($categories as $category) {
-                                            $h++;
-                                        }
-                                        $h = $h - 1;
-                                        $i = 0;
-                                        foreach ($categories as $category) {
-                                            $result = "";
-                                            if ($i < $h) {
-                                                $result .= $category->name . ", ";
-                                            } else {
-                                                $result .= $category->name;
+                    <div class="content-row">
+                        <div class="col-sma-1">&nbsp;</div>
+                        <div class="col-sma-10">
+                            <h3>Serving Gardners and Landscapers Since 1982</h3>
+                            <p>We've had the honor of selling plants and landscaping and gardening supplies to our customers for many years.
+                                Drop and by and see why garderners and landscapers make us their go-to garden and landscaping store!</p>
+                        </div>
+                        <div class="col-sma-1">&nbsp;</div>
+                    </div>	
+                    <div class="content-row">
+                        <div class="col-sma-12">
+                            <?php
+                            global $post;
+                            $args = array('posts_per_page' => 3);
+                            $postsToDisplay = get_posts($args);
+                            foreach ($postsToDisplay as $post) : setup_postdata($post);
+                                ?>      
+                                <div class="col-sma-4">
+                                    <div class="blog-post">
+                                        <a href="/our-blog"><h4 class="blog-post__title"><?php the_title(); ?></h4></a>
+                                        <div class="blog__categories"><?php
+                                            $categories = get_the_category();
+                                            $h = 0;
+                                            foreach ($categories as $category) {
+                                                $h++;
                                             }
-                                            echo $result;
-                                            $i++;
-                                        }
-                                        ?>
+                                            $h = $h - 1;
+                                            $i = 0;
+                                            foreach ($categories as $category) {
+                                                $result = "";
+                                                if ($i < $h) {
+                                                    $result .= $category->name . ", ";
+                                                } else {
+                                                    $result .= $category->name;
+                                                }
+                                                echo $result;
+                                                $i++;
+                                            }
+                                            ?>
+                                        </div>
+                                        <div class="blog__date"><?php the_date(); ?></div>
+                                        <div class="blog__image"><a href="/our-blog"><?php the_post_thumbnail(); ?></a></div>
+                                        <div class="blog__content"><?php the_excerpt(); ?></div>
+                                        <div class="clear-both"></div>
                                     </div>
-                                    <div class="blog__date"><?php the_date(); ?></div>
-                                    <div class="blog__image"><a href="/our-blog"><?php the_post_thumbnail(); ?></a></div>
-                                    <div class="blog__content"><?php the_excerpt(); ?></div>
-                                    <div class="clear-both"></div>
                                 </div>
-                            </div>
-                        <?php endforeach; ?>
+                            <?php endforeach; ?>
+                        </div>
                     </div>
                 </div>
             </div>		
             <div class="subfooter-container">
-                <div class="nursery-view__subheader"><h3>You could be here looking at plants!</h3></div>
-                <div class="subfooter-container__background content__content-image"></div>
+                <div class="inner-wrapper">
+                    <div class="nursery-view__subheader"><h3>You could be here looking at plants!</h3></div>
+                    <div class="subfooter-container__background content__content-image"></div>
+                </div>
             </div>
             @include('includes.footer')
             <script>
