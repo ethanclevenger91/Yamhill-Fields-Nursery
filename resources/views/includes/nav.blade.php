4b733e36bb1a039531fee652a5cb11f8585902d5
Logan Testa <logan.testa@outlook.com>
2019-12-14 16:57:56 -0800

    Adjust header, footer, index page images, styling

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,6 +1,24 @@
+  
 <nav class="nav mobile-nav" id="mobileNav">
-    <div id="dropdownButton"></div>
-    <div id="dropdownContent">
+    <div class="inner-wrapper">
+        <div id="dropdownButton"></div>
+        <div id="dropdownContent">
+            <ul class="nav__nav-list">
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/index">Home</a>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/plants">Plants</a>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/supplies">Supplies</a>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/our-blog">Blog</a>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/about-us">About</a>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/contact-us">Contact</a>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+
+<nav class="nav desktop-nav" id="desktopNav">
+    <div class="inner-wrapper">
         <ul class="nav__nav-list">
             <li class="nav__nav-item"><a class="nav__nav-link" href="/index">Home</a>
             <li class="nav__nav-item"><a class="nav__nav-link" href="/plants">Plants</a>
@@ -9,25 +27,5 @@
             <li class="nav__nav-item"><a class="nav__nav-link" href="/about-us">About</a>
             <li class="nav__nav-item"><a class="nav__nav-link" href="/contact-us">Contact</a>
         </ul>
-	</div>
+    </div>
 </nav>
-
-<nav class="nav desktop-nav" id="desktopNav">
-    <ul class="nav__nav-list">
-        <li class="nav__nav-item"><a class="nav__nav-link" href="/index">Home</a>
-        <li class="nav__nav-item"><a class="nav__nav-link" href="/plants">Plants</a>
-        <li class="nav__nav-item"><a class="nav__nav-link" href="/supplies">Supplies</a>
-        <li class="nav__nav-item"><a class="nav__nav-link" href="/our-blog">Blog</a>
-        <li class="nav__nav-item"><a class="nav__nav-link" href="/about-us">About</a>
-        <li class="nav__nav-item"><a class="nav__nav-link" href="/contact-us">Contact</a>
-    </ul>
-</nav>
-
-
-
-
-
-
-
-
-
