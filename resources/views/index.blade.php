13291b78f996b937a0e808ee9dc41f66eecfb468
Logan Testa <logan.testa@outlook.com>
2020-01-23 23:37:52 -0800

    Improve blog image styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -101,7 +101,7 @@
                                             ?>
                                         </div>
                                         <div class="blog__date"><?php the_date(); ?></div>
-                                        <div class="blog__image"><a href="/our-blog"><?php the_post_thumbnail( 'thumbnail' ); ?></a></div>
+                                        <div class="blog__image"><a href="/our-blog"><?php the_post_thumbnail( 'thumbnail' ); ?></a> <div class="clear-both"></div></div>
                                         <div class="blog__content"><?php the_excerpt(); ?></div>
                                         <div class="clear-both"></div>
                                     </div>
